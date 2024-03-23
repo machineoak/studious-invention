46675ae6a4e383cfe34341fa2a07988f0f4497be
keven carvalho <dangerdasedicoes@gmail.com>
2024-03-23 00:10:20 -0300

    luaproj1

---
+++ main.lua
@@ -0,0 +1,21 @@
+-- weapon
+
+--[[
+	fr my mannnnnnn
+	--]]
+
+    --[[
+    local bolsonAro = "a regular text"
+
+    local block
+    
+    local function infinityLoop(block)
+    end
+
+    --]]
+
+function onClick()
+    print("none")
+    end
+    
+    script.Parent.MouseButton1Click:Connect(onClick)
